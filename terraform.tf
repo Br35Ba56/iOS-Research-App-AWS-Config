cd93eecece82c126bd3cf9972166641a9b2fa730
Anthony Schneider <tonyschndr@gmail.com>
2018-09-23 21:35:33 -0500

    added cognito_get_id policy document

--- terraform.tf
+++ terraform.tf
@@ -39,17 +39,12 @@ resource "aws_cognito_user_pool_client" "client" {
 }
 
 resource "aws_cognito_identity_pool" "main" {
-  identity_pool_name               = "identity pool"
+  identity_pool_name               = "NFP_Breastfeeding_Identity_Pool"
   allow_unauthenticated_identities = true
-  //This needs to get changed to cognito user pool app client id
-cognito_identity_providers {
-  provider_name = "cognito-idp.us-east-1.amazonaws.com/${aws_cognito_user_pool_client.client.user_pool_id}"
-  client_id = "${aws_cognito_user_pool_client.client.id}"
-}
-
-  /*supported_login_providers {
-    "graph.facebook.com" = "7346241598935555"
-  }*/
+  cognito_identity_providers {
+    provider_name = "cognito-idp.us-east-1.amazonaws.com/${aws_cognito_user_pool_client.client.user_pool_id}"
+    client_id = "${aws_cognito_user_pool_client.client.id}"
+  }
 }
 
 resource "aws_iam_role" "authenticated" {
@@ -99,6 +94,12 @@ resource "aws_iam_role_policy" "authenticated_cognito_sync" {
   ]
 }
 EOF
+}
+
+resource "aws_iam_role_policy" "authenticated_cognito_get_id" {
+  name = "cognito_get_id"
+  role = "${aws_iam_role.authenticated.id}"
+  policy = "${data.aws_iam_policy_document.cognito_get_id.json}"
 }
 
 resource "aws_iam_role_policy" "authenticated_s3_put_object" {
@@ -163,7 +164,7 @@ resource "aws_cognito_identity_pool_roles_attachment" "main" {
   identity_pool_id = "${aws_cognito_identity_pool.main.id}"
 
   role_mapping {
-    identity_provider         = "graph.facebook.com"
+    identity_provider         = "cognito-idp.us-east-1.amazonaws.com/${aws_cognito_user_pool_client.client.user_pool_id}:${aws_cognito_user_pool_client.client.id}"
     ambiguous_role_resolution = "AuthenticatedRole"
     type                      = "Rules"
 
@@ -177,5 +178,16 @@ resource "aws_cognito_identity_pool_roles_attachment" "main" {
 
   roles {
     "authenticated" = "${aws_iam_role.authenticated.arn}"
+  }
+}
+
+data "aws_iam_policy_document" "cognito_get_id" {
+  statement {
+    actions = [
+      "cognito-identity:GetId"
+    ]
+    resources = [
+      "arn:aws:cognito-identity:*:*:identityPool/${aws_cognito_identity_pool.main.id}",
+    ]
   }
 }
