3a3fcd357d6f56fb12034bc9c6d334df71176043
Anthony Schneider <tonyschndr@gmail.com>
2018-11-14 12:13:38 -0600

    Added " to output values

--- terraform_out_to_app_config.py
+++ terraform_out_to_app_config.py
@@ -13,7 +13,8 @@ with open('./NFPAppConfig/AWS_Config.swift', 'r') as myfile:
   data = myfile.read()
 
 for key in output_values.keys():
-    data = data.replace(key.strip(), output_values[key])
+    value = output_values[key].strip()
+    data = data.replace(key.strip(), "\"" + value + "\"")
 
 file = open('./NFPAppConfig/DoNotCommit.swift', 'w')
 file.write(data)
