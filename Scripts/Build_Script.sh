106b5d96a079f77c6eb1f2f3729b96bad0baeb4d
Anthony Schneider <tonyschndr@gmail.com>
2018-11-19 20:42:44 -0600

    Added logic to create tfvar if does not exist.

--- Scripts/Build_Script.sh
+++ Scripts/Build_Script.sh
@@ -1,28 +1,35 @@
-#/bin/bash
+#!/bin/bash
 
-#Remove var.tf for clean configuration should use environment variable for
-#checking for production or testing deployment
-#rm terraform.tfvar
-#touch terraform.tfvar;
-#echo "pinpointID=id" > terraform.tfvar
+#Check if terrafrom.tfvar exists and has valid pinpoint app
 
-#Create pinpoint app
-#pinpointID=$(aws pinpoint create-app --create-application-request Name="NFP_Breastfeeding_App" | grep Id );
-#IFS=':' read -ra ID <<< "$pinpointID";
-#echo "${ID[1]//,}";
+if [ ! -e ~/Research/Cloud/Terraform/terraform.tfvar ]; then
+  echo ""
+  echo "pinpointID=id" > terraform.tfvar
+  
+  #Create pinpoint app
+  pinpoint_id_raw=$(aws pinpoint create-app --create-application-request Name="NFP_Breastfeeding_App" | grep Id );
+  IFS=':' read -ra ID <<< "$pinpoint_id_raw";
+  echo "${ID[1]//,}";
 
-#Replaces id in var.tf
-#sed -i '' -e "s/id/${ID[1]//,}/g" terraform.tfvar
+  #Replaces id in var.tf
+  sed -i '' -e "s/id/${ID[1]//,}/g" ~/Research/Cloud/Terraform/terraform.tfvar
 
-#Remove " and , from string
-#PINPOINTID=$(echo ${ID[1]//,} | tr -d \")
+  #Remove " and , from string
+  pinpoint_id=$(echo ${ID[1]//,} | tr -d \")
 
-terraform apply -var-file=terraform.tfvar
+else 
+  echo "file exists"
+fi
 
-terraform_output=$(terraform output);
+#execute terraform in initialized terraform directory
+(
+  cd ~/Research/Cloud/Terraform/; 
+  terraform apply -var-file=~/Research/Cloud/Terraform/terraform.tfvar; 
+  terraform_output=$(terraform output); 
+  #Parses output file into swift file for iOS App backend
+  python3 ~/Research/Cloud/Scripts/terraform_out_to_app_config.py "$terraform_output";
+)
 
-python3 terraform_out_to_app_config.py "$terraform_output"
+sam package --template-file ~/Research/Cloud/sam-app/template.yaml --s3-bucket serverlesssambucket123454321 --output-template-file ~/Research/Cloud/sam-app/packaged.yaml
 
-sam package --template-file ./sam-app/template.yaml --s3-bucket serverlesssambucket123454321 --output-template-file ./sam-app/packaged.yaml
-
-aws cloudformation deploy --template-file ./sam-app/packaged.yaml --stack-name nfpbreastfeedingserverlessstack --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file ~/Research/Cloud/sam-app/packaged.yaml --stack-name nfpbreastfeedingserverlessstack --capabilities CAPABILITY_IAM
--- Scripts/terraform_out_to_app_config.py
+++ Scripts/terraform_out_to_app_config.py
@@ -9,13 +9,13 @@ output_values = dict(item.split("=") for item in terraform_variables.split("\n")
 
 data = ""
 
-with open('./NFPAppConfig/AWS_Config.swift', 'r') as myfile:
+with open('../NFPAppConfig/AWS_Config.swift', 'r') as myfile:
   data = myfile.read()
 
 for key in output_values.keys():
     value = output_values[key].strip()
     data = data.replace(key.strip(), "\"" + value + "\"")
 
-file = open('./NFPAppConfig/DoNotCommit.swift', 'w')
+file = open('../NFPAppConfig/DoNotCommit.swift', 'w')
 file.write(data)
 file.close()
