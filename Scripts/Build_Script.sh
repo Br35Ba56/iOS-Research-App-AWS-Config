d9701728f4a3a7ac7bfffc5d6c0a866c469e8839
Anthony Schneider <tonyschndr@gmail.com>
2018-11-21 09:34:26 -0600

    Updated Scripts

--- Scripts/Build_Script.sh
+++ Scripts/Build_Script.sh
@@ -1,35 +1,46 @@
 #!/bin/bash
 
-#Check if terrafrom.tfvar exists and has valid pinpoint app
 
-if [ ! -e ~/Research/Cloud/Terraform/terraform.tfvar ]; then
-  echo ""
-  echo "pinpointID=id" > terraform.tfvar
+terraform_dir=~/Research/Cloud/Terraform/
+terraform_out_to_app_config=~/Research/Cloud/Scripts/terraform_out_to_app_config.py
+sam_template=~/Research/Cloud/sam-app/template.yaml
+sam_package_cf=~/Research/Cloud/sam-app/packaged.yaml
+
+#Check if terrafrom.tfvar exists and has valid pinpoint app
+if [ ! -e "$terraform_dir"terraform.tfvar ]; then
+  touch ~/Research/Cloud/Terraform/terraform.tfvar
+  echo "pinpointID=id" > "$terraform_dir"terraform.tfvar
   
   #Create pinpoint app
-  pinpoint_id_raw=$(aws pinpoint create-app --create-application-request Name="NFP_Breastfeeding_App" | grep Id );
-  IFS=':' read -ra ID <<< "$pinpoint_id_raw";
+  pinpoint_create_app_result=$(aws pinpoint create-app --create-application-request Name="NFP_Breastfeeding_App" | grep Id );
+
+  #echo result of 
+  echo "$pinpoint_create_app_result"
+  IFS=':' read -ra ID <<< "$pinpoint_create_app_result";
   echo "${ID[1]//,}";
 
   #Replaces id in var.tf
-  sed -i '' -e "s/id/${ID[1]//,}/g" ~/Research/Cloud/Terraform/terraform.tfvar
+  sed -i '' -e "s/id/${ID[1]//,}/g" "$terraform_dir"terraform.tfvar
 
   #Remove " and , from string
   pinpoint_id=$(echo ${ID[1]//,} | tr -d \")
 
 else 
-  echo "file exists"
+  echo "aws pinpoint app id already exists"
 fi
 
 #execute terraform in initialized terraform directory
 (
-  cd ~/Research/Cloud/Terraform/; 
-  terraform apply -var-file=~/Research/Cloud/Terraform/terraform.tfvar; 
+  cd $terraform_dir; 
+  terraform apply -var-file="$terraform_dir"terraform.tfvar; 
   terraform_output=$(terraform output); 
   #Parses output file into swift file for iOS App backend
-  python3 ~/Research/Cloud/Scripts/terraform_out_to_app_config.py "$terraform_output";
+  python3 $terraform_out_to_app_config "$terraform_output";
 )
 
-sam package --template-file ~/Research/Cloud/sam-app/template.yaml --s3-bucket serverlesssambucket123454321 --output-template-file ~/Research/Cloud/sam-app/packaged.yaml
+sam package --template-file $sam_template --s3-bucket serverlesssambucket123454321 --output-template-file $sam_package_cf
 
-aws cloudformation deploy --template-file ~/Research/Cloud/sam-app/packaged.yaml --stack-name nfpbreastfeedingserverlessstack --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file $sam_package_cf --stack-name nfpbreastfeedingserverlessstack --capabilities CAPABILITY_IAM
+
+aws pinpoint delete-app --application-id $pinpoint_id
+rm ~/Research/Cloud/Terraform/terraform.tfvar
--- Scripts/terraform_out_to_app_config.py
+++ Scripts/terraform_out_to_app_config.py
@@ -1,21 +1,26 @@
 #!/usr/local/bin/python3
 
+import os
 import sys
 import string
 
+
 terraform_variables = sys.argv[1]
+config_file = os.path.expanduser("~/Research/Cloud/NFPAppConfig/AWS_Config.swift")
+output_file = os.path.expanduser("~/Research/iOS-Research-App/Breast Feeding NFP/Login/DoNotCommit.swift")
 
 output_values = dict(item.split("=") for item in terraform_variables.split("\n"))
 
 data = ""
 
-with open('../NFPAppConfig/AWS_Config.swift', 'r') as myfile:
+with open(config_file, 'r') as myfile:
   data = myfile.read()
 
 for key in output_values.keys():
     value = output_values[key].strip()
     data = data.replace(key.strip(), "\"" + value + "\"")
 
-file = open('../NFPAppConfig/DoNotCommit.swift', 'w')
+#Write output to iOS project
+file = open(output_file, 'w')
 file.write(data)
 file.close()
